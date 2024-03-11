5715bb7a162fcf98a4476b5613b5184fe460abdf
dhomas1 <dom@cotugno.co>
2024-03-11 15:43:53 -0400

    Update service.sh
    Update version number to 9.6p1

--- src/dest/service.sh
+++ src/dest/service.sh
@@ -7,7 +7,7 @@
 
 framework_version="2.1"
 name="openssh"
-version="9.2p1"
+version="9.6p1"
 description="OpenSSH is a suite of security-related network-level utilities based on the Secure Shell protocol."
 depends=""
 webui=""
