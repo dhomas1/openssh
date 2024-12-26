c42b5dad43d2e3b974d90838d2c4e8a2db105922
dhomas1 <dom@cotugno.co>
2024-12-26 10:26:09 -0500

    Update app.sh
    change lib folder for openssl

--- app.sh
+++ app.sh
@@ -27,7 +27,7 @@ local URL="https://github.com/kzalewski/openssl-1.1.1/archive/refs/tags/${FILE}"
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/openssl-1.1.1-1.1.1zb_p1"
 ./configure --prefix="${DEPS}" --openssldir="${DEST}/etc/ssl" \
-  zlib-dynamic --with-zlib-include="${DEPS}/include" --with-zlib-lib="${DEPS}/lib" \
+  zlib-dynamic --with-zlib-include="${DEPS}/include" --with-zlib-lib="${DEPS}/include" \
   shared threads linux-armv4 -DL_ENDIAN ${CFLAGS} ${LDFLAGS} \
   -Wa,--noexecstack -Wl,-z,noexecstack
 sed -i -e "s/-O3//g" Makefile
